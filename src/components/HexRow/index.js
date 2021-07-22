c2a97c883d427e7220ec29b408bfc9f62f31177d
Zachary Reid <zrreid97@gmail.com>
2021-07-22 13:31:47 -0400

    Updated the underlay to emphasive the glow.

--- src/components/HexRow/index.js
+++ src/components/HexRow/index.js
@@ -4,28 +4,8 @@ import HexTile from '../HexTile';
 
 export default function HexRow() {
 
-    const generateTiles = (width) => {
-        console.log('Window Width:', width)
-        let tiles = [];
-        for (let i=0; i<(width/38); i++) {
-            tiles.push(<HexTile key={i}/>);
-        }
-
-        return tiles;
-    }
-
     return (
         <div className='hexRow'>
-            <HexTile className='backdrop'/>
-            <HexTile className='backdrop'/>
-            <HexTile className='backdrop'/>
-            <HexTile className='backdrop'/>
-            <HexTile className='backdrop'/>
-            <HexTile className='backdrop'/>
-            <HexTile className='backdrop'/>
-            <HexTile className='backdrop'/>
-            <HexTile className='backdrop'/>
-            <HexTile className='backdrop'/>
         </div>
     );
 }
--- src/components/HexTile/index.js
+++ src/components/HexTile/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+//clip-path: polygon(50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%, 0 25%);
+
 export default function HexTile() {
     return (
         <div className='hexTile'></div>
