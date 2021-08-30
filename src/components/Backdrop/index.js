759621acc59e7130c35579134f7b7fbdc8fad59d
Zachary Reid <zrreid97@gmail.com>
2021-08-30 13:50:59 -0400

    Setting a point from which I will refactor the whole project.

--- src/components/Backdrop/index.js
+++ src/components/Backdrop/index.js
@@ -3,17 +3,6 @@ import React from 'react';
 import './Backdrop.scss';
 
 export default function Backdrop() {
-    // return (
-    //     <div className='backdrop'>
-    //         <div className='circle1'>
-    //             <div className='spoke'></div>
-    //             <div className='spoke'></div>
-    //             <div className='spoke'></div>
-    //         </div>
-    //     </div>
-        
-    // );
-
     return (
         <div className='hexSheet'>
             <div className='overlay'></div>
