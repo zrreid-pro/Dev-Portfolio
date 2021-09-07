4ec6a9c9f7895689cf1a472d4c7f8038163e42d3
Zachary Reid <zrreid97@gmail.com>
2021-09-07 01:11:05 -0400

    Created a rough draft of the site and an in-progress version of the sidebar.

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Header } from './Header';
 export { default as HexTile } from './HexTile';
 export { default as Backdrop } from './Backdrop';
 export { default as HexBar } from './HexBar';
+export { default as MainContent } from './MainContent';
+export { default as Sidebar } from './Sidebar';
